c32ff530e70e371a1b53c09042653adb4e40068d
Ivan Trotsenko <ivantrotsenko@Ivans-MacBook-Air.local>
2025-04-07 22:34:02 +0200

    Fixed js link

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import wrangle_data
+from wrangling_scripts.wrangle_data import return_figures
 import plotly
 import json
 
@@ -8,17 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def dashboard():
     # Get list of figure dicts from your wrangle_data.py
-    figures = wrangle_data.return_figures()
+    figures = return_figures()
 
     # Convert Plotly dicts to JSON for embedding in HTML
-    ids = [f'chart-{i}' for i, _ in enumerate(figures)]
-    chart_json = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
 
     # Pass figure JSON and chart IDs to the template
     return render_template(
         'index.html',
         ids=ids,
-        chartJSON=chart_json
+        figuresJSON=figuresJSON
     )
 
 if __name__ == '__main__':
